4e40c4ed73cd01f85c1412536631358389445945
marcgantenbein <ma.gantenbein@gmail.com>
2019-11-01 13:36:59 +0100

    removed matplotlib dependency for running on biwi

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 from torch.autograd import Variable
 import torch.nn.functional as F
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 def truncated_normal_(tensor, mean=0, std=1):
     size = tensor.shape
@@ -35,11 +35,11 @@ def l2_regularisation(m):
             l2_reg = l2_reg + W.norm(2)
     return l2_reg
 
-def save_mask_prediction_example(mask, pred, iter):
-	plt.imshow(pred[0,:,:],cmap='Greys')
-	plt.savefig('images/'+str(iter)+"_prediction.png")
-	plt.imshow(mask[0,:,:],cmap='Greys')
-	plt.savefig('images/'+str(iter)+"_mask.png")
+# def save_mask_prediction_example(mask, pred, iter):
+# 	plt.imshow(pred[0,:,:],cmap='Greys')
+# 	plt.savefig('images/'+str(iter)+"_prediction.png")
+# 	plt.imshow(mask[0,:,:],cmap='Greys')
+# 	plt.savefig('images/'+str(iter)+"_mask.png")
 
 def makefolder(folder):
     '''
